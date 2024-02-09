25383b0539ed664e9683d0e505d5fb6e237a298e
Bay40k <bay40k@gmail.com>
2024-02-08 18:24:40 -0700

    fix credits markdown

--- pages/Credits.js
+++ pages/Credits.js
@@ -9,7 +9,13 @@ import remarkGfm from "remark-gfm";
 
 // The component that takes markdown content as a prop
 export default function MarkdownCreditsAccordion() {
-  return (
+    const content = `
+- *bay40k (me)*
+- *psp2wpp creator* - ***[https://github.com/Princess-of-Sleeping/psp2wpp](https://github.com/Princess-of-Sleeping/psp2wpp)***
+- */u/Havocking1992* for some value descriptions and tutorials
+- *smokeanthrax*
+`
+    return (
     <Accordion className="creditsAccordion" type="single" collapsible>
       <AccordionItem value="credits">
         <AccordionTrigger className="creditsAccordionText">
@@ -17,9 +23,7 @@ export default function MarkdownCreditsAccordion() {
         </AccordionTrigger>
         <AccordionContent>
           <Markdown className="creditsMarkdown" remarkPlugins={[remarkGfm]}>
-            - *bay40k (me)*
-            - *psp2wpp creator* - ***[https://github.com/Princess-of-Sleeping/psp2wpp](https://github.com/Princess-of-Sleeping/psp2wpp)***
-            - */u/Havocking1992* for some value descriptions and tutorials
+            {content}
           </Markdown>
         </AccordionContent>
       </AccordionItem>
